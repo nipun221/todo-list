3564eee863e6f5c62be96c08e31b52fb5e4b4fb9
nipun221 <2oics072@gbu.ac.in>
2023-07-15 00:21:04 +0530

    Final Commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {v4 as uuidv4} from "uuid";
 
-const Form = ({input, setInput, todos, setTodos}) => {
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+
+    const updateTodo = (title, id, completed) => {
+        const newTodo = todos.map((todo) => 
+            todo.id === id ? {title, id, completed} : todo
+        );
+        setTodos(newTodo);
+        setEditTodo("");
+    };
+
+    useEffect(() => {
+        if(editTodo) {
+            setInput(editTodo.title);
+        } else {
+            setInput("");
+        }
+    }, [setInput, editTodo]);
 
     const onInputChange = (e) => {
         setInput(e.target.value);
@@ -9,8 +25,13 @@ const Form = ({input, setInput, todos, setTodos}) => {
 
     const onFormSubmit = (e) => {
         e.preventDefault();
-        setTodos([...todos, {id: uuidv4(), title: input, completed: false}]);
+        if(!editTodo){
+            setTodos([...todos, {id: uuidv4(), title: input, completed: false}]);
         setInput("");
+        } else {
+            updateTodo(input, editTodo.id, editTodo.completed);
+        }
+        
     };
 
   return (
@@ -23,7 +44,9 @@ const Form = ({input, setInput, todos, setTodos}) => {
             required
             onChange={onInputChange}
         />
-        <button className='button-add' type='submit'>Add</button>
+        <button className='button-add' type='submit'>
+            {editTodo ? "OK" : "Add"}
+        </button>
     </form>
   );
 };
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
-const TodosList = ({todos, setTodos}) => {
+const TodosList = ({ todos, setTodos, setEditTodo }) => {
+
+    const handleComplete = (todo) => {
+        setTodos(
+            todos.map((item) => {
+                if (item.id === todo.id) {
+                    return {...item, completed: !item.completed};
+                }
+                return item;
+            })
+        );
+    };
+
+    const handleEdit = ({id}) => {
+        const findTodo = todos.find((todo) => todo.id === id);
+        setEditTodo(findTodo);
+    };
+
+    const handleDelete = ({id}) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
   return (
     <div>
         {todos.map((todo) => (
@@ -8,18 +29,18 @@ const TodosList = ({todos, setTodos}) => {
                 <input 
                     type='text' 
                     value={todo.title} 
-                    className='list' 
+                    className={`list ${todo.completed ? "complete" : ""}`}
                     onChange={(e) => e.preventDefault()} 
                 />
                 <div>
-                    <button className='button-complete task-button'>
-                        <i className='fa fa-check-circle'></i>
+                    <button className='button-complete task-button' onClick={()=> handleComplete(todo)}>
+                        <i className="fa fa-check-circle" aria-hidden="true"></i>
                     </button>
-                    <button className='button-edit task-button'>
-                        <i className='fa fa-edit'></i>
+                    <button className='button-edit task-button' onClick={()=> handleEdit(todo)}>
+                        <i className='fa fa-edit' aria-hidden="true"></i>
                     </button>
-                    <button className='button-delete task-button'>
-                        <i className='fa fa-trash'></i>
+                    <button className='button-delete task-button' onClick={()=> handleDelete(todo)}>
+                        <i className='fa fa-trash' aria-hidden="true"></i>
                     </button>
                 </div>
             </li>
