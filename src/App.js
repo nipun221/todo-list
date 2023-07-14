8e09528cc072e3ec0d0926ab572683aac9652f6c
nipun221 <2oics072@gbu.ac.in>
2023-07-14 22:33:04 +0530

    created UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodosList from './components/TodosList';
 
-function App() {
+const App = () => {
+
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div>App</div>
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form 
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+          />
+        </div>
+        <div>
+          <TodosList todos={todos} setTodos={setTodos} />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
